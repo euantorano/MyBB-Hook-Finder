573ce2146294ce18e333289306d67dcefa2ce45a
Euan T <euantorano@gmail.com>
2013-05-22 00:12:41 +0100

    Adding command line options and tidying.

--- README.md
+++ README.md
@@ -4,4 +4,21 @@ Locate hooks within MyBB including arguments passed to the hook and the file/lin
 
 ## Running this script
 
-This script should be placed in the root of a MyBB install and ran as any other ruby script should be ran. The output is stored in HTML format in output.html. If you're on windows, your browser should open the new file as soon as it's finished being written. I have not tested this on any other platform though.
+This script should be ran via the command line like so:
+
+	ruby mybbhooks.rb
+
+### Options
+
+You can optionally pass a few options into the script when running it via command line. You can view details of the available options at any time by passing the -h flag:
+
+	ruby mybbhooks.rb -h
+
+This will detail all available options such as below:
+
+	Usage: mybbhooks.rb [options]
+		-p, --path PATH                  Specify a path to the MyBB root. Defaults to current directory.
+		-o, --output FILE                Specify an output file. Defaults to output.html.
+		-h, --help                       Display this help screen
+
+If you do not pass any arguments to the script, it will run ony any and all PHP scripts in the current directory and it's children and will store the output in output.html.
--- src/mybbhooks.rb
+++ src/mybbhooks.rb
@@ -1,8 +1,32 @@
 #!/usr/bin/env ruby
 
+# Command line arguments
+require 'optparse'
+
+options = {}
+OptionParser.new do |opts|
+	opts.banner = "Usage: mybbhooks.rb [options]"
+
+	options[:mybb_root] = ""
+	opts.on( '-p', '--path PATH', 'Specify a path to the MyBB root. Defaults to current directory.' ) do |mybb_path|
+		options[:mybb_root] = mybb_path.strip.chomp("\\").chomp("/")
+	end
+
+	options[:output_file] = "output.html"
+	opts.on( '-o', '--output FILE', 'Specify an output file. Defaults to output.html.' ) do |output_file|
+		options[:output_file] = output_file
+	end
+
+	opts.on( '-h', '--help', 'Display this help screen' ) do
+		puts opts
+		exit
+	end
+end.parse!
+
+# Hook parsing
 hooks = Hash.new
 
-Dir.glob("**/*.php") do |mybb_file|
+Dir.glob(options[:mybb_root] + "/**/*.php") do |mybb_file|
 	file = File.open(mybb_file, "r")
 	line_no = 0
 	while !file.eof?
@@ -14,7 +38,7 @@ Dir.glob("**/*.php") do |mybb_file|
 	end
 end
 
-fileOutput = File.new("output.html", "w+")
+fileOutput = File.new(options[:output_file], "w+")
 fileOutput.puts "<!doctype html>"
 fileOutput.puts "<html lang=\"en-GB\">"
 fileOutput.puts "	<head>"
@@ -46,4 +70,4 @@ fileOutput.puts "	</body>"
 fileOutput.puts "</html>"
 fileOutput.close
 
-system("start output.html")
+system("start " + options[:output_file])
