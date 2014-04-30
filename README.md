54e13f5fd0802ddadd212eb6c37b324018ceb7a6
Euan Torano <euantorano@gmail.com>
2014-04-30 20:24:29 +0100

    Cleaning up code and adding comments
    Removing output file, shouldn’t be under VCS. Also updating README.

--- README.md
+++ README.md
@@ -6,27 +6,27 @@ Locate hooks within MyBB including arguments passed to the hook and the file/lin
 
 This script should be ran via the command line like so:
 
-	ruby mybbhooks.rb
+```bash
+chmod +x src/mybb_hook_finder.rb
+./src/mybb_hook_finder.rb
+```
 
 ### Options
 
 You can optionally pass a few options into the script when running it via command line. You can view details of the available options at any time by passing the -h flag:
 
-	ruby mybbhooks.rb -h
+```bash
+./src/mybb_hook_finder.rb -h
+```
 
 This will detail all available options such as below:
 
-	Usage: mybbhooks.rb [options]
+```
+	Usage: mybb_hook_finder.rb [options]
 		-p, --path PATH                  Specify a path to the MyBB root. Defaults to current directory.
 		-o, --output FILE                Specify an output file. Defaults to output.html.
+		-n, --nogroup                    Don't group hooks by file.
 		-h, --help                       Display this help screen
+```
 
-If you do not pass any arguments to the script, it will run ony any and all PHP scripts in the current directory and it's children and will store the output in output.html.
-
-## Finding hooks by file
-
-Hooks are also grouped by file within the file_hooks map. They're not currently outputted, but you can access them by simply adding:
-
-	puts file_hooks
-
-I'll be modifying the output script soon so you can optionally group by file.
+If you do not pass any arguments to the script, it will run ony any and all PHP scripts in the current directory and it's children and will store the output in ```src/output.html```.
---
+++ src/mybb_hook_finder.rb
@@ -0,0 +1,140 @@
+#!/usr/bin/env ruby
+
+# Command line arguments
+require 'optparse'
+
+options = {}
+OptionParser.new do |opts|
+	opts.banner = "Usage: mybb_hook_finder.rb [options]"
+
+	options[:mybb_root] = ""
+	opts.on( '-p', '--path PATH', 'Specify a path to the MyBB root. Defaults to current directory.' ) do |mybb_path|
+		options[:mybb_root] = mybb_path.strip.chomp("\\").chomp("/") + "/"
+	end
+
+	options[:output_file] = "output.html"
+	opts.on( '-o', '--output FILE', 'Specify an output file. Defaults to output.html.' ) do |output_file|
+		options[:output_file] = output_file
+	end
+
+	options[:group_output] = true
+	opts.on( '-n', '--nogroup', 'Don\'t group hooks by file.' ) do
+		options[:group_output] = false
+	end
+
+	opts.on( '-h', '--help', 'Display this help screen' ) do
+		puts opts
+		exit
+	end
+end.parse!
+
+# Hook parsing
+hooks = Hash.new
+file_hooks = Hash.new{|hash, key| hash[key] = Array.new}
+
+# Loop through every *.php file in the mybb directory and read it
+Dir.glob(options[:mybb_root] + "**/*.php") do |mybb_file|
+	File.open(mybb_file, "r") do |file| # Open the file for reading
+		line_no = 0
+		file_name = mybb_file[options[:mybb_root].length, mybb_file.length - options[:mybb_root].length]
+		while !file.eof? # Loop through every line of the file looking for matches - have to do it this way to get the line number
+			line = file.readline
+			line_no += 1
+			if (line =~ /\$plugins\->run_hooks\(((["']?)([a-zA-Z\-_0-9]*)(["']?))([,]?)([ \$a-zA-Z0-9]*?)\);/i) # Does the lien contains a $plugins->run_hooks() call? If so, we've found a hook!
+				hooks[$3.strip] = [$6.strip, mybb_file, line_no]
+				file_hooks[file_name].push([$3.strip, $6.strip, line_no])
+			end
+		end
+	end
+end
+
+File.open(options[:output_file], "w+") do |file| # Write the output file!
+	file.puts %{<!doctype html>
+<html lang="en-GB">
+	<head>
+		<title>MyBB Hooks</title>
+		<style>
+			* {
+				margin: 0;
+				padding: 0;
+			}
+
+			body {
+				padding: 4px;
+				overflow-y: scroll;
+				font-family: Tahoma, Verdana, Segoe, sans-serif;
+			}
+
+			table {
+				margin: 8px 4px 4px;
+				width: 100%;
+				border: 1px solid rgba(0, 0, 0, 0.2);
+			}
+
+			td, th {
+				padding: 4px;
+				border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+			}
+
+			tbody tr:last-child td {
+				border-bottom: 0;
+			}
+
+			tr.file th {
+				background: rgba(0, 0, 0, 0.1);
+			}
+
+			tr.heading td {
+				font-weight: bold;
+			}
+		</style>
+	</head>
+	<body>
+		<h1>MyBB Hooks</h1>
+		<table>}
+
+	if (options[:group_output])
+		i = 0
+		file_hooks.each_pair do |key, value|
+			file.puts %{<tr class="file">
+					<th colspan="3" class="tcat"><strong>File: </strong> #{key}</th>
+				</tr>
+				<tr class="heading">
+					<td>Hook</td>
+					<td>Params</td>
+					<td>Line</td>
+				</tr>}
+
+			file_hooks[key].each do |subVal|
+				file.puts %{<tr>
+						<td>#{subVal[0]}</td>
+						<td>#{subVal[1]}</td>
+						<td>#{subVal[2]}</td>
+					</tr>}
+			end
+
+			i += 1
+		end
+else
+	hooks.each do |key, value|
+		file.puts %{<tr>
+				<td><strong>Hook: </strong>#{key}</td>
+				<td>}
+
+		if (!value[0].empty?)
+			file.puts "<strong>Params: </strong>#{value[0]}"
+		end
+
+		file.puts %{</td>
+				<td><strong>File / Line </strong>#{value[1]} / #{value[2]}</td>
+			</tr>}
+
+	end
+end
+
+		file.puts %{</table>
+	</body>
+</html>}
+end
+
+system("start " + options[:output_file])
